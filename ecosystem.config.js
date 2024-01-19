e76983b81d870ef8e72badcda631d5624bdebe6a
unknown <mobifone@DESKTOP-S59VM5C.mobifone.vn>
2024-01-19 11:59:55 +0700

    update

---
+++ ecosystem.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  apps: [
+    {
+      script: "index.js",
+      watch: ".",
+    },
+    {
+      script: "./service-worker/",
+      watch: ["./service-worker"],
+    },
+  ],
+
+  deploy: {
+    production: {
+      key: "key-pair-name.pem", // pem file generated when launching an instance
+      user: "ec2-user", // ec2-user if you use aws linux kernel, ubuntu if you use ubuntu kernel
+      host: "13.212.240.246", // public ip address of the ec2 instance
+      ref: "main", // specify the branch where your code resides
+      repo: "https://github.com/byline2009/node-ai-next.git", //ssh git url of your repo
+      path: "/home/ec2-user/ai-node-next/node-ai-next", // mention the path in ec2 instance where your code need to be eg /home/ec2-user
+      "pre-deploy-local": "",
+      "post-deploy":
+        "source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+      ssh_options: "ForwardAgent=yes",
+    },
+  },
+};
