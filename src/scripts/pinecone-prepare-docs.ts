f38ba0bf60be938f64d75429b1186d84992b5b9b
unknown <mobifone@DESKTOP-S59VM5C.mobifone.vn>
2024-02-21 23:17:12 +0700

    update

--- src/scripts/pinecone-prepare-docs.ts
+++ src/scripts/pinecone-prepare-docs.ts
@@ -1,13 +1,21 @@
 import { getChunkedDocsFromPDF } from "@/lib/pdf-loader";
 import { embedAndStoreDocs } from "@/lib/vector-store";
 import { getPineconeClient } from "@/lib/pinecone-client";
+import { env } from "@/lib/config";
 
 // This operation might fail because indexes likely need
 // more time to init, so give some 5 mins after index
 // creation and try again.
+
 (async () => {
   try {
     const pineconeClient = await getPineconeClient();
+    const index = pineconeClient.Index(env.PINECONE_INDEX_NAME);
+    await index.delete1({
+      deleteAll: true,
+      namespace: env.PINECONE_NAME_SPACE,
+    });
+    console.log("delete all");
     console.log("Preparing chunks from PDF file");
     const docs = await getChunkedDocsFromPDF();
     console.log(`Loading ${docs.length} chunks into pinecone...`);
