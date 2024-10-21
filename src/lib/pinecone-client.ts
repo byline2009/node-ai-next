5155574994381cff903dfec70b442432ab3ae5c7
unknown <mobifone@DESKTOP-S59VM5C.mobifone.vn>
2024-10-21 16:08:15 +0700

    update

--- src/lib/pinecone-client.ts
+++ src/lib/pinecone-client.ts
@@ -14,7 +14,7 @@ const client = new ProxyAgent({
 const customFetch = (input: string | URL | Request, init: any) => {
   return fetch(input, {
     ...init,
-    // dispatcher: client as any,
+    dispatcher: client as any,
     keepalive: true,
   });
 };
@@ -29,19 +29,19 @@ const pc = new Pinecone(config);
 // Create pineconeIndex if it doesn't exist
 async function createIndex(client: Pinecone, indexName: string) {
   try {
-     await pc.createIndex({
-    name: env.PINECONE_INDEX_NAME,
+    await pc.createIndex({
+      name: env.PINECONE_INDEX_NAME,
 
-    // should match embedding model name, e.g. 3072 for OpenAI text-embedding-3-large and 1536 for OpenAI text-embedding-ada-002
-    dimension: 3072,
-    metric: "cosine",
-    spec: {
-      serverless: {
-        cloud: "aws",
-        region: "us-east-1",
+      // should match embedding model name, e.g. 3072 for OpenAI text-embedding-3-large and 1536 for OpenAI text-embedding-ada-002
+      dimension: 3072,
+      metric: "cosine",
+      spec: {
+        serverless: {
+          cloud: "aws",
+          region: "us-east-1",
+        },
       },
-    },
-  });
+    });
 
     console.log(
       `Waiting for 240000 seconds for index initialization to complete...`
@@ -61,7 +61,7 @@ async function initPineconeClient() {
     const isExistedPC = await checkIndexExists(pc);
     if (!isExistedPC) {
       console.log("Index is not existed");
-     await createIndex(pc, indexName);
+      await createIndex(pc, indexName);
     } else {
       console.log("Your index already exists. nice !!");
     }
@@ -72,14 +72,14 @@ async function initPineconeClient() {
   }
 }
 
-async function checkIndexExists(pc : Pinecone) {
+async function checkIndexExists(pc: Pinecone) {
   // List all indexes
   const response = await pc.listIndexes();
-  const indexes =  response.indexes;
-  console.log('Available indexes:', indexes)
+  const indexes = response.indexes;
+  console.log("Available indexes:", indexes);
 
   // Check if the desired index is in the list
-  return indexes?.find(item => item.name === env.PINECONE_INDEX_NAME);
+  return indexes?.find((item) => item.name === env.PINECONE_INDEX_NAME);
 }
 
 export async function getPineconeClient() {
