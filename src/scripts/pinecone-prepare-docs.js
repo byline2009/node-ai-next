911effcfd1d6aa862380068ffe9995689e750b5c
unknown <mobifone@DESKTOP-S59VM5C.mobifone.vn>
2024-10-21 16:35:08 +0700

    update

--- src/scripts/pinecone-prepare-docs.js
+++ src/scripts/pinecone-prepare-docs.js
@@ -9,14 +9,30 @@ import { env } from "@/lib/config";
 
 (async () => {
   try {
-    const pineconeClient = await getPineconeClient();
-    const index = pineconeClient.index(env.PINECONE_INDEX_NAME);
-    console.log("Preparing chunks from PDF file");
-    const docs = await getChunkedDocsFromPDF();
-    console.log(`Loading ${docs.length} chunks into pinecone...`);
-    const embedData = await embedDocs(pineconeClient, docs);
-    console.log("Data embedded and stored in pine-cone index");
-    await storeEmbeddings(pineconeClient, embedData);
+    // const pineconeClient = await getPineconeClient();
+    // const index = pineconeClient.index(env.PINECONE_INDEX_NAME);
+    // console.log("Preparing chunks from PDF file");
+    // const docs = await getChunkedDocsFromPDF();
+    // console.log(`Loading ${docs.length} chunks into pinecone...`);
+    // const embedData = await embedDocs(pineconeClient, docs);
+    // console.log("Data embedded and stored in pine-cone index");
+    // await storeEmbeddings(pineconeClient, embedData);
+
+    const chat = new ChatOpenAI(
+      { temperature: 0, openAIApiKey: env.OPENAI_API_KEY },
+      { basePath: "http://10.39.152.30:3128" }
+    );
+
+    try {
+      const response = await chat.call([
+        new HumanChatMessage(
+          "Translate this sentence from English to French. I love programming."
+        ),
+      ]);
+      console.log(response);
+    } catch (e) {
+      console.log(e);
+    }
   } catch (error) {
     console.error("Init client script failed ", error);
   }
